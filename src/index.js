7c2b65eca1da847201da20d90a57bad36fcbd0b8
mcPiper <66990552+mcPiper@users.noreply.github.com>
2021-08-28 10:38:30 +0300

    init: update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 function eval() {
-    // Do not use eval!!!
+   
     return;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  expr = expr.replace(/ /g, ''); // remove all spaces
+  if (expr.indexOf('/0') !== -1)
+   
+    throw new Error('TypeError: Division by zero.');
+
+  
+  let brackets = 0;
+  expr.split('').forEach((el) => {
+    el === '(' ? brackets++ : false;
+    el === ')' ? brackets-- : false;
+  });
+
+  if (brackets !== 0)
+   
+    throw new Error('ExpressionError: Brackets must be paired');
+
+
+  expr = new Function('return ' + expr);
+
+  return expr();
 }
 
 module.exports = {
